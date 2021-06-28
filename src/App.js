431da477a13e0705733ea7a331003ec8a2812d7f
markjohnnah <markjohnnah@switchmaven.com>
2021-06-28 23:16:23 +1000

    adds grommet header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import './App.css';
-import { Grommet } from 'grommet';
+
+import { Grommet, Menu, Header, Button, Card, CardBody, CardHeader, CardFooter  } from 'grommet';
+import { Home, Favorite, ShareOption } from 'grommet-icons';
 
 const theme = {
   global: {
     font: {
-      family: 'Roboto',
-      size: '18px',
-      height: '20px',
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
     },
   },
 };
 
 function App() {
   return (
-    <div className="App">
-       <Grommet theme={theme}>
-      <header className="App-header">      
-        <h1>My Site</h1>
-        <p>I'm blabla...</p>
-       
-      </header>
+    <div>
+      <Grommet theme={theme}>
+        <header className="App-header">
+          <Header background="brand">
+            <Button icon={<Home />} hoverIndicator />
+            <Menu label="account" items={[{ label: "logout" }]} />
+          </Header>
+        </header>
+        <Card  height="small" width="large" background="light-1">
+      <CardHeader pad="medium">Welcome</CardHeader>
+      <CardBody pad="medium">About Me</CardBody>
+      <CardFooter pad={{horizontal: "small"}} background="light-2">   
+      <Button
+      icon={<Favorite color="red" />}
+      hoverIndicator
+    />
+    <Button icon={<ShareOption color="plain" />} hoverIndicator />
+    </CardFooter>
+</Card>
       </Grommet>
     </div>
-   
   );
 }
 
